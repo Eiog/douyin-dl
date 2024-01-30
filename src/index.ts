6b9f7c01c7a35348d58287045009a1473e41a396
antrioe <antrioe@gmali.com>
2024-01-30 17:21:56 +0800

    fix(components): fix request

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ interface DouyinVideoDataResult {
   status_code: number
 }
 const { get } = objectPath
-const shareRegex = /http[s]?:\/\/(douyin.com|v.douyin.com)\/(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+/g
+const shareRegex = /http[s]?:\/\/(douyin.com|v.douyin.com)\/(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+/
 const videoDataUrl = (videoId: string) => `https://www.douyin.com/aweme/v1/web/aweme/detail/?device_platform=webapp&aid=6383&channel=channel_pc_web&aweme_id=${videoId}&pc_client_type=1&version_code=190500&version_name=19.5.0&cookie_enabled=true&screen_width=1344&screen_height=756&browser_language=zh-CN&browser_platform=Win32&browser_name=Firefox&browser_version=110.0&browser_online=true&engine_name=Gecko&engine_version=109.0&os_name=Windows&os_version=10&cpu_core_num=16&device_memory=&platform=PC&webid=7158288523463362079&msToken=abL8SeUTPa9-EToD8qfC7toScSADxpg6yLh2dbNcpWHzE0bT04txM_4UwquIcRvkRb9IU8sifwgM1Kwf1Lsld81o9Irt2_yNyUbbQPSUO8EfVlZJ_78FckDFnwVBVUVK`
 const homePageUrl = (secUid: string, maxCursor: number) => `https://www.douyin.com/aweme/v1/web/aweme/post/?sec_user_id=${secUid}&count=35&max_cursor=${maxCursor}&aid=1128&version_name=23.5.0&device_platform=android&os_version=2333`
 const headers = {
@@ -49,10 +49,10 @@ export function getRealUrlByShortUrl(url: string): Promise<string> {
         return resolve(redirectUrl as string)
       }
       else {
-        return reject(res.toString())
+        return reject(res)
       }
     }).catch((err) => {
-      return reject(err.toString())
+      return reject(err)
     })
   })
 }
@@ -81,11 +81,9 @@ export function getDouyinVideoDataByVideoId(videoId: string): Promise<DouyinVide
   return new Promise((resolve, reject) => {
     const newApiUrl = generateXBogusUrl(videoDataUrl(videoId))
     fetch(newApiUrl, { headers: douyinApiHeaders }).then(res => res.json()).then((res) => {
-      if (res.data)
-        return resolve(res.data)
-      return reject(res.toString())
+      return resolve(res)
     }).catch((error: any) => {
-      return reject(error.toString())
+      return reject(error)
     })
   })
 }
@@ -124,14 +122,17 @@ export function getHomeVideoBySecUid(secUid: string): Promise<any> {
 export function getInfo(url: string) {
   return new Promise((resolve, reject) => {
     if (!validateUrl(url))
-      return reject('分享链接错误')
+      return reject('Link validation failed')
+
     const shortUrl = getShortUrlByShareUrl(url)
     if (!shortUrl)
-      return reject('分享链接错误')
+      return reject('Failed to get short link')
+
     getRealUrlByShortUrl(shortUrl).then((res) => {
       const videoId = getDouyinVideoIdByRealUrl(res)
       if (!videoId)
-        return reject('videoId fill')
+        return reject('Failed to get ID')
+
       getDouyinVideoDataByVideoId(videoId).then((res) => {
         resolve(res)
       }).catch((error: any) => {
