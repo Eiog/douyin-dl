00321234cc46f427223546b88f4efb61436758a1
antrioe <antrioe@gmali.com>
2024-02-02 17:23:54 +0800

    feat(components): feat download

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 /* eslint-disable prefer-promise-reject-errors */
+import { Readable } from 'node:stream'
 import * as objectPath from 'object-path'
-
 import { sign } from '../public/x-bogus.cjs'
 
 interface DouyinVideoDataResult {
@@ -8,6 +8,7 @@ interface DouyinVideoDataResult {
   log_pb: { [key: string]: any }
   status_code: number
 }
+
 const { get } = objectPath
 const shareRegex = /http[s]?:\/\/(douyin.com|v.douyin.com)\/(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+/
 const videoDataUrl = (videoId: string) => `https://www.douyin.com/aweme/v1/web/aweme/detail/?device_platform=webapp&aid=6383&channel=channel_pc_web&aweme_id=${videoId}&pc_client_type=1&version_code=190500&version_name=19.5.0&cookie_enabled=true&screen_width=1344&screen_height=756&browser_language=zh-CN&browser_platform=Win32&browser_name=Firefox&browser_version=110.0&browser_online=true&engine_name=Gecko&engine_version=109.0&os_name=Windows&os_version=10&cpu_core_num=16&device_memory=&platform=PC&webid=7158288523463362079&msToken=abL8SeUTPa9-EToD8qfC7toScSADxpg6yLh2dbNcpWHzE0bT04txM_4UwquIcRvkRb9IU8sifwgM1Kwf1Lsld81o9Irt2_yNyUbbQPSUO8EfVlZJ_78FckDFnwVBVUVK`
@@ -30,6 +31,8 @@ export function setCookie(cookie: string) {
  * @returns boolean
  */
 export const validateUrl = (url: string) => shareRegex.test(url)
+export const getVideo = (data: DouyinVideoDataResult): string[] | null => get(data, 'aweme_detail.video.play_addr.url_list')
+export const getCover = (data: DouyinVideoDataResult): string[] | null => get(data, 'aweme_detail.video.origin_cover.url_list')
 export function getShortUrlByShareUrl(url: string) {
   const match = url.match(shareRegex)
   if (match && match?.length > 0)
@@ -119,7 +122,7 @@ export function getHomeVideoBySecUid(secUid: string): Promise<any> {
     })
   })
 }
-export function getInfo(url: string) {
+export function getInfo(url: string): Promise<DouyinVideoDataResult> {
   return new Promise((resolve, reject) => {
     if (!validateUrl(url))
       return reject('Link validation failed')
@@ -140,6 +143,58 @@ export function getInfo(url: string) {
       })
     }).catch((error: any) => {
       return reject(error.toString())
+    })
+  })
+}
+function fetchVideo(url: string): Readable {
+  const stream = new Readable({
+    read: () => true,
+  })
+  fetch(url, { headers }).then(async (res) => {
+    const contentLength = Number(res.headers.get('Content-Length')) || 0
+    let receivedLength = 0
+    if (!res.body)
+      throw new Error('stream failed')
+    const reader = res.body.getReader()
+    while (true) {
+      const { value, done } = await reader.read()
+      if (done) {
+        stream.push(null)
+        stream.emit('end', { size: contentLength })
+        break
+      }
+      receivedLength += value.length
+      stream.push(value)
+      stream.emit('process', { current: receivedLength, size: contentLength, process: parseFloat((receivedLength / contentLength).toFixed(2)) })
+    }
+  }).catch((error) => {
+    stream.emit('error', error)
+  })
+  return stream
+}
+export function downloadFromInfo(data: DouyinVideoDataResult) {
+  const videoUrl = getVideo(data)
+  let current = 0
+  if (!Array.isArray(videoUrl) || videoUrl.length === 0)
+    throw new Error('video url failed')
+  function fetchStream(url: string) {
+    return fetchVideo(url)
+  }
+  const stream = fetchStream(videoUrl[current])
+  stream.on('error', () => {
+    current += 1
+    if (current >= videoUrl.length - 1)
+      throw new Error('video url failed')
+    fetchStream(videoUrl[current])
+  })
+  return stream
+}
+export function download(url: string): Promise<Readable> {
+  return new Promise((resolve, reject) => {
+    getInfo(url).then((res) => {
+      return resolve(downloadFromInfo(res))
+    }).catch((error) => {
+      return reject(error)
     })
   })
 }
